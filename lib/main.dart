1ce591966afb25d3482f8c69410b43bded090182
Yareni Mérida <yareni@grupocybac.com>
2022-05-02 18:15:53 -0500

    Cambios desde el trabajo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
 import 'package:app1/pages/home.dart';
 import 'package:app1/pages/login.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+const AndroidNotificationChannel chanel = AndroidNotificationChannel(
+  'high_importance_channel',
+  'High Importance Notifications',
+  importance: Importance.high,
+  playSound: true,
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> firebaseMessassingBackground(RemoteMessage message) async {
+  print("Inicializacion del mensaje");
+  await Firebase.initializeApp();
+  print("A bg message just showed ${message.messageId}");
+}
 
 Future<void> main() async {
+  print("Main de inicio");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
